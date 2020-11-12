3cf9fa400340f4394e8fc5f7191325ca4e2e5d49
AnaCristinaCorona <70342405+AnaCristinaCorona@users.noreply.github.com>
2020-11-12 14:53:45 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,174 @@
-
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    music.baDing.play()
+    otherSprite.destroy()
+    sprite.startEffect(effects.hearts, 200)
+    info.changeScoreBy(500)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.powerDown.play()
+    scene.cameraShake(4, 500)
+    otherSprite.destroy(effects.blizzard, 500)
+    sprite.startEffect(effects.rings, 200)
+    info.changeLifeBy(-1)
+    info.changeScoreBy(-250)
+})
+let projectile: Sprite = null
+let projectile2: Sprite = null
+effects.confetti.startScreenEffect()
+let Junk_Food = [img`
+    ......................bbb.......
+    ....................bb333b......
+    .................bbb333d33b.....
+    ................bb333333d3a.....
+    ..............bb33332eeeedba....
+    ............bbb333323eee2e3a....
+    ..........bbd333333e22222ed3a...
+    .......bbbdd3333333e22222edda...
+    ......bb3d333333333be222eb3d3a..
+    ...bbb3dd33333333333beeeb33d3a..
+    ..b3ddd33333333333333333333dda..
+    bbddd3333333333333333333333dd3a.
+    b33dddddd3333333333333333333d3a.
+    bb3333333ddddd33333333333333dda.
+    bbbbbbb333dd33dddddddddd3333ddba
+    b55553bbbbbb3333dd33333ddd33dd3a
+    b555555555553bbbbbbbb33333dddd3a
+    bd555555555555555dddbaaaaab3d3ba
+    bb55555555555555555dddddddbb33ba
+    b3bb35555555555d5555d55dddddbbba
+    b33333bbb355dd55555d555ddddddbba
+    b5555d333333bbb35dddddd55ddddbba
+    b5d555dd5553333bbbbb3ddddddddb3a
+    b5d555555555555dd3333bbbbbb3db3a
+    bd5d555555d55555dd555ddbbbbbbb3a
+    bbb55dd555555555555555ddddddbb3a
+    ...bbbbdd555ddd5555ddddddddddb3a
+    .......bbbb555555d5ddd5ddddddb3a
+    ...........bbbb55555555555dd533a
+    ...............bbbbddd5d55d5b3ba
+    ...................bbbbddddb3ba.
+    .......................bbbaaaa..
+    `, img`
+    ..............bbbbbbb...........
+    ...........bb66663333baa........
+    .........bb3367776333663aa......
+    ........b33333888333389633aa....
+    .......b3333333333333389633aa...
+    ......b34443333333333338633bae..
+    .....b3455433333333334443333ae..
+    ....b33322333dddd3333455233daee.
+    ...b3d333333dd3bbbb33322333dabe.
+    ..b3d333333d3bb33bb33333333da4e.
+    ..bd33333333b33aab3333333223a4ee
+    .b3d3663333b33aab33366332442b4ee
+    .bd3b983333a3aa3333387633ee3b4ee
+    .bd6983333baaa333333387633bb4bee
+    b3d6833333bba333333333863ba44ebe
+    bdd3333333bb3333333333333a44bebe
+    add666633333322333366333ba44bbbe
+    ad67776333332442336983d3a444b4e.
+    add888b333333ee3369833d3a44b44e.
+    add333333333333336833d3a444b4e..
+    a3dd3333344433333dddd3a444b44e..
+    ab33ddd325543333dd33aa444b44e...
+    .eabb3dd32233333baaa4444b44e....
+    .ebabb3d333d33baa444443b44e.....
+    ..ebaab3ddd3aaa4444433b44e......
+    ..eebbaab33a44444333b444e.......
+    ...eeebbaab444b333b4444e........
+    ....ebeeebbbbbbbb4444ee.........
+    .....eebbbb44444444ee...........
+    .......eeebbb444eee.............
+    ..........eeeeee................
+    ................................
+    `, img`
+    ............3333bb..bb33333.....
+    ........3bb31111d3b311d111d33...
+    .......3bdd11111dbd11d11111113..
+    .......bdddd1111bd11d111dd11113.
+    ......3d111dd111b11d111dd33d11d3
+    ......3d11111dd1d11d111d11d33113
+    ....bb3d111111dd13dd111d1dd3b31b
+    ...b3d3dd111111dd13dd11d1dddbbdb
+    ...3ddd31d111111dd133dddddddb.b.
+    ..311111d1ddd1111dd11dddddd33...
+    ..3111111d111dd111dd1111dd3313..
+    ..bddd1111ddd11dd111d111111113..
+    ..311ddd111dddd11dd11ddd1111ddb.
+    ..31111dd111dddd11dd111dddddddb.
+    ...bd1111d1113ddd11dd1111111d3b.
+    ...4dd1111d1113ddd11ddd111d333b.
+    ..4dbdddd11d11133ddddddddddddb..
+    .4ddbddddd11d111d33ddddddddd3b..
+    .4dddb11ddd11dd111d333dddd3bb...
+    .4dd55b111d11dd11111d3333bbb....
+    .445555b111d11dddd111111ddb.....
+    .4455555bd1d311ddddddddddd3.....
+    .45455555bb1d3111ddddddd113.....
+    .4554555555b333d1111111113......
+    455554555555bbb33d11111d33......
+    4d555545555555dbbb3d11d33.......
+    4dd5555455555ddddd43333.........
+    45dd555544ddddddd4..............
+    .45dd5555d44dddd4...............
+    ..45dd55dddd4444................
+    ...45dd55444....................
+    ....44444.......................
+    `]
+let Panda = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . . . 
+    c c c c c d d e e e f c . . . . 
+    . f d d d d e e e f f . . . . . 
+    . . f e e e f f e e e f . . . . 
+    . . f f f f f e e e e e f . f f 
+    . . f d b f e e f f e e f . e f 
+    . f f d d f e f f e e e f . e f 
+    . f f f f f f e b b f e f f e f 
+    . f d d f e e e d d b e f f f f 
+    . . f f f f f f f f f f f f f . 
+    `, SpriteKind.Player)
+let Cherry = [img`
+    . . . . . . . . . . . 6 6 6 6 6 
+    . . . . . . . . . 6 6 7 7 7 7 8 
+    . . . . . . 8 8 8 7 7 8 8 6 8 8 
+    . . e e e e c 6 6 8 8 . 8 7 8 . 
+    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+    e 2 e e 2 2 2 2 e e e e c 6 8 . 
+    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+    . . . e c c e c 2 2 2 2 2 2 2 e 
+    . . . . . . . c 2 e e 2 2 e 2 c 
+    . . . . . . . c e e e e e e 2 c 
+    . . . . . . . . c e 2 2 2 2 c . 
+    . . . . . . . . . c c c c c . . 
+    `]
+Panda.setPosition(74, 95)
+Panda.setFlag(SpriteFlag.StayInScreen, true)
+info.setLife(3)
+info.setScore(0)
+game.onUpdateInterval(5000, function () {
+    projectile2 = sprites.createProjectileFromSide(Cherry[randint(0, Cherry.length - 1)], 0, 75)
+    projectile2.setKind(SpriteKind.Food)
+    projectile2.x = randint(10, 100)
+})
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(Junk_Food[randint(0, Junk_Food.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
+forever(function () {
+    if (controller.right.isPressed()) {
+        Panda.x += 5
+    } else if (controller.left.isPressed()) {
+        Panda.x += -5
+    }
+})
